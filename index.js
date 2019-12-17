904946ed508e19b934676ce10b869777e2da17a1
Felipe Cardozo <fcrcardozo@outlook.com>
2019-12-17 00:44:30 -0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = document.getElementById('teste').innerHTML;
+
+let b = document.querySelector('p')
+
+
+let c = document.getElementsByClassName("place")[0].innerHTML;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+document.getElementsByClassName("place")[0].innerHTML = "<h1> oi </h1>";
